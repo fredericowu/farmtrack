d7685506f3f305146ebd8d32c58e6a62b3dea077
Bruno Melo <brmelo.leite@gmail.com>
2019-10-20 10:14:21 -0300

    template global para itens não encontrados

--- storytelling/js/Ingredientes/IngredientesController.js
+++ storytelling/js/Ingredientes/IngredientesController.js
@@ -10,13 +10,17 @@ const IngredientesController = (function(){
         const url = new URL(location.href);
         const pratoId = url.searchParams.get("prato");
         if(!pratoId){
-            $("#ingredientes").html(IngredientesTemplates.nenhumIngrediente());
+            $("#ingredientes").html(TemplateGlobal.itemNaoEncontrado());
             return;
         }
 
         new Promise((res, rej) => {
             $.get("/api/produtos/detalhes/?id="+pratoId, res);
         }).then((ingredientes) => {
+            if(ingredientes.length == 0){
+                $("#ingredientes").html(TemplateGlobal.itemNaoEncontrado());
+                return;
+            }
             for(let ingrediente of ingredientes){
                 if(ingrediente.fornecedor) fornecedores.push(ingrediente.fornecedor);
                 $("#ingredientes").append(IngredientesTemplates.item(ingrediente));
